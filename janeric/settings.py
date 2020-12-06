35f92110d1984ec6ffdc6b1d182b049223984506
sherylg343 <sheryl.s.goldberg@gmail.com>
2020-12-06 17:33:04 +0000

    styling of products template and fixed media url

--- janeric/settings.py
+++ janeric/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 #allauth needs this from django
                 'django.contrib.auth.context_processors.auth', 
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.media",
             ],
         },
     },
