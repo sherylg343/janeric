da00bd3788eb285c4ca86c29cc0c36962cb36e96
sherylg343 <sheryl.s.goldberg@gmail.com>
2020-12-07 16:36:01 +0000

    setting up view for product detail page and search field

--- products/views.py
+++ products/views.py
@@ -1,24 +1,56 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 
 
-# Create your views here.
+from .models import Product, Category, Product_Family
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
     products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            categories = Category.objects.filter(name__in=categories)
+            products = products.filter(category__name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria.")
+                return redirect(reverse('products'))
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
+    product_family = None
+    family_products = None
+
     product = get_object_or_404(Product, pk=product_id)
+    product_family = product.product_family
+    family_products = Product.objects.filter(
+        product_family=product_family)
 
     context = {
         'product': product,
+        'family_products': family_products,
+        'product_family': product_family,
     }
+
     return render(request, 'products/product_detail.html', context)
