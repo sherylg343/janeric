23cfc1e50bcb1d902d0560c9f4848ac1050f4b1d
sherylg343 <sheryl.s.goldberg@gmail.com>
2020-12-07 23:20:34 +0000

    more work on cart context and template

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django .shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contents(request):
@@ -7,12 +9,24 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
+    for product_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=product_id)
+        total = quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'product_id': product_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+
+    if total < settings.FREE_SHIPPING_THRESHOLD:
         shipping = total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE/100)
         free_shipping_delta = settings.FREE_SHIPPING_THRESHOLD - total
     else:
-        delivery = 0
+        shipping = 0
         free_shipping_delta = 0
 
     grand_total = shipping + total
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,20 @@ def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, product_id):
+    """ Add a quantity of the specified product to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if product_id in list(cart.keys()):
+        cart[product_id] += quantity
+    else:
+        cart[product_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
