ff9e9f9f0dcbceb404d7ffadff43c3e397d9c810
sherylg343 <sheryl.s.goldberg@gmail.com>
2020-12-21 04:25:20 +0000

    fixed indent issue in settings for dbase

--- janeric/settings.py
+++ janeric/settings.py
@@ -131,12 +131,12 @@ if 'DATABASE_URL' in os.environ:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
