37a36aa706c4fcaa7611e4186d17d05df682355f
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-01-04 13:33:59 +0000

    sticky side-menu and other formatting changes

--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from phone_field import PhoneField
-
 
 class UserProfile(models.Model):
     """
@@ -19,7 +17,8 @@ class UserProfile(models.Model):
     defaultship_city = models.CharField(max_length=40, null=True, blank=True)
     defaultship_state = models.CharField(max_length=80, null=True, blank=True)
     defaultship_zipcode = models.CharField(max_length=20, null=True, blank=True)
-    defaultship_phone_number = PhoneField(null= True, blank=True)
+    defaultship_phone_number = models.CharField(
+        max_length=20, null= True, blank=True)
 
     def __str__(self):
         return self.user.username
