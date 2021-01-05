fc4d0af640c14c427ab16c74e927073a18b0f2e3
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-01-05 21:51:41 +0000

    changes to checkout.js

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -1,20 +1,20 @@
-let shipFullName = $('#id_ship_full_name');
-let shipStreet1 = $('#id_ship_street_address1');
-let shipStreet2 = $('#id_ship_street_address2');
-let shipCity =  $('#id_ship_city');
-let shipState =  $('#id_ship_state');
-let shipZipCode = $('#id_ship_zipcode');
-let shipPhone = $('#id_ship_phone_number');
-let billFullName = $('#id_bill_full_name');
-let billStreet1 = $('#id_bill_street_address1');
-let billStreet2 = $('#id_bill_street_address2');
-let billCity = $('#id_bill_city');
-let billState = $('#id_bill_state');
-let billZipCode = $('#id_bill_zipcode');
-let billPhone = $('#id_bill_phone_number');
+const shipFullName = $('#id_ship_full_name');
+const shipStreet1 = $('#id_ship_street_address1');
+const shipStreet2 = $('#id_ship_street_address2');
+const shipCity =  $('#id_ship_city');
+const shipState =  $('#id_ship_state');
+const shipZipCode = $('#id_ship_zipcode');
+const shipPhone = $('#id_ship_phone_number');
+const billFullName = $('#id_bill_full_name');
+const billStreet1 = $('#id_bill_street_address1');
+const billStreet2 = $('#id_bill_street_address2');
+const billCity = $('#id_bill_city');
+const billState = $('#id_bill_state');
+const billZipCode = $('#id_bill_zipcode');
+const billPhone = $('#id_bill_phone_number');
 const $steps = $('.step'); 
 
-$(document).ready(function () {
+$(document).ready(function() {
     console.log("function working");
     //MD Bootstrap payment stepper
     $('.stepper').mdbStepper();
