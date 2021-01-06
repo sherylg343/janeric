634c4d2891d142a819c5d43708ecda7a2b22a8d6
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-01-06 16:18:34 +0000

    added debugger in checkout.js

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -2,17 +2,15 @@ $(document).ready(function() {
     console.log("function working");
     //MD Bootstrap payment stepper
     $('.stepper').mdbStepper();
-
+debugger;
     /* Add gray to SelectState when page loads and change when selected */
-    $('#id_ship_state').css('color', '#c3ccd3');
-
-    $('#id_ship_state').change(function() {
+    $('#id_ship_state').on('change', function() {
         $('#id_ship_state').css('color', '#47646f');
     });
 
     $('#id_bill_state').css('color', '#c3ccd3');
 
-    $('#id_bill_state').change(function() {
+    $('#id_bill_state').on('change', function() {
         $('#id_bill_state').css('color', '#47646f'); 
     });
 });
