011a9445b0ddd491018021348e72cb5cc3ccaff9
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-01-08 15:02:05 +0000

    changed to add product form

--- products/forms.py
+++ products/forms.py
@@ -7,8 +7,6 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        model = Product_Family
-        model = Category
         fields = '__all__'
 
     image = forms.ImageField(
@@ -18,15 +16,11 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         cat_names = [(c.id, c.get_name()) for c in categories]
-        div_names = [(c.id, c.get_division()) for c in categories]
         product_families = Product_Family.objects.all()
         pf_names = [(pf.id, pf.get_name()) for pf in product_families]
-        brand_names = [(pf.id, pf.get_brand_name()) for pf in product_families]
 
         self.fields['category'].choices = cat_names
-        self.fields['division'].choices = div_names
         self.fields['product_family'].choices = pf_names
-        self.fields['brand_name'].choices = brand_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-border rounded-0'
 
